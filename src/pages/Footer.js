24cc6c40a3da5666d4b75d300b78980b8b9ec8d5
Thomas Rye <thomasjrye@gmail.com>
2023-10-18 15:11:36 +0200

    Fix link to Instagram profile in Footer.js component

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
       <footer className='Footer'>
         <div class="column" id="first">
           <a href= "https://www.linkedin.com/in/thomas-rye-b08064121/" class="fa fa-linkedin"></a>
-          <a href= "https://www.instagram.com/jacobsenrye/" class="fa fa-instagram"></a>
+          <a href= "https://www.instagram.com/thomasjacobsenrye/" class="fa fa-instagram"></a>
           <a href= "https://twitter.com/JacobsenRye" class="fa fa-twitter"></a>
           <a href= "https://github.com/ThomasJRye" class="fa fa-github"></a>
           <a href= "mailto: thomasjrye@gmail.com" class="fa fa-envelope"></a>
